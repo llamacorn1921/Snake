15549e9b473602920ae0124bfd8089b5c33e410b
Llama <40404790+llamacorn1921@users.noreply.github.com>
2022-02-11 14:30:34 -0500

    j

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ requirejs.config({
 });
 require(['jquery', 'game'], function ($, game) {
 	/** Test if scripts have loaded */
-	const version = '0.1.30';
+	const version = '0.1.31';
 	const canvas = document.getElementById('scene');
 	/** sets all canvas info for other scripts */
 	let cInfo = { 
