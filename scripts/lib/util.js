ddaca048313af91e5675c419c62d9657cbdf98af
Llama <40404790+llamacorn1921@users.noreply.github.com>
2022-02-11 12:49:49 -0500

    14

--- scripts/lib/util.js
+++ scripts/lib/util.js
@@ -1,22 +1,19 @@
 define(function() {
 	const timerID = document.getElementById('timer');
 	let sec = 15;
-	function countdown () {
+	function time (apple) {
+		sec--;
+		if (sec === 0) {
+			sec = 15;
+			apple.destory()
+			timerID.innerHTML = '00:00';
+		}
+		timerID.innerHTML = `00:${ sec.toString().padStart(2, '0') }`;
 	}
 
 	return {
 		timer: function (apple) {
-			setInterval(() => {
-				sec--
-				if (sec < 10) {
-					
-				}
-				timerID.innerHTML = `00:${ sec.padStart(2, 0) }`;
-				if (sec === 0) {
-					sec = 15;
-					apple.destroy();
-				}
-			}, 1000);
+			setInterval(time(apple), 1000);
 		}
 	}
 });
