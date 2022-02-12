870a5c9b195b4cbcc1512b523eb6c518d7fb7459
Llama <40404790+llamacorn1921@users.noreply.github.com>
2022-02-12 01:55:58 -0500

    update for the night

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ A simple snake game using html5 canvas
 		or at least an attemp
 
 I know the code is messy
+
+Whaat I am using:
+	jquery
+	requirejs
+	Reverb.js: http://reverbjs.org (http://reverbjs.org/reverb.min.js)
+		NOTE: I had to take the file and edit the beginning of it so it would work with requirejs
+	node:
+		uglify to make stuff alot faster
--- scripts/lib/audio.js
+++ scripts/lib/audio.js
@@ -1,1 +1,1 @@
-define(["./src/reverb"],function(n){const e=document.getElementById("munch");return{init:function(){console.log(n)},play:function(){e.pause();e.currentTime=0;e.play()}}});
+define(["../src/reverb"],function(n){let e=n;const t=document.getElementById("munch");let i;function o(){e.extend(i)}return{init:function(){i=new AudioContext;console.log("Audio Script init loaded")},play:function(){t.pause();t.currentTime=.1;t.play()}}});
